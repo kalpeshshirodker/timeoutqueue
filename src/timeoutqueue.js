b80d54bb92922a9741c06ca70292c7cb3d3a4a89
kalpeshshirodker <kalpesh.shirodker@gmail.com>
2015-09-24 17:43:09 +0530

    Added Callbacks for after all functions have completed

--- src/timeoutqueue.js
+++ src/timeoutqueue.js
@@ -13,14 +13,49 @@ var timeoutQueue = (function(){
 			throw "Invalid object: Function expected";
 		}
 	}
-	tqproto.execute = function(timeout, scope){
+	tqproto.execute = function(timeout, scope, fnCallback){		
+		var me = this;
 		if(!this.synchronous){
-			if(this.fnQueue && this.fnQueue.length > 0){
+			if(me.fnQueue && me.fnQueue.length > 0){
 				var fn;
-				scope = scope || this;
-				while(this.fnQueue.length > 0){
-					fn = this.fnQueue.shift();
-					setTimeout(fn.bind(scope), timeout);
+				scope = scope || me;
+				
+				var fnCnt = me.fnQueue.length;
+				var fnExecCmpl = {
+					success : 0,
+					fail : 0
+				};
+				
+				var cb = function(isSuccess){
+					if(isSuccess){
+						fnExecCmpl.success++;
+					}
+					else{
+						fnExecCmpl.fail++;
+					}
+					console.log(fnExecCmpl);
+					if(fnCnt === (fnExecCmpl.success + fnExecCmpl.fail)){
+						//all functions have executed
+						if(fnCallback){
+							fnCallback.call(scope, fnExecCmpl);
+						}
+					}
+				}
+				while(me.fnQueue.length > 0){
+					fn = me.fnQueue.shift();
+					//setTimeout(fn.bind(scope), timeout);
+					(function(fn, scope){
+						setTimeout(function(){
+							try{
+								fn.call(scope);
+								cb(true);
+							}
+							catch(e){
+								cb(false);
+								throw e;
+							}
+						}, timeout);
+					})(fn, scope);
 				}	
 			}
 			else{
@@ -28,7 +63,7 @@ var timeoutQueue = (function(){
 			}
 		}
 		else{
-			this.executeSync(timeout, scope);
+			me.executeSync(timeout, scope);
 		}
 	}
 	tqproto.executeSync = function(timeout, scope){
