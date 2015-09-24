c5cbaf9966ed866d9d942b4746f2d802d227e669
kalpeshshirodker <kalpesh.shirodker@gmail.com>
2015-09-24 18:06:53 +0530

    Execute functions in provided scope

--- src/timeoutqueue.js
+++ src/timeoutqueue.js
@@ -1,15 +1,23 @@
 var timeoutQueue = (function(){
 	var tq = function(synchronous){
-		this.fnQueue = [];
+		this.fnQueue = {
+			fnDef : []
+		};
 		this.synchronous = synchronous || false;
 	}
 	
 	var tqproto = tq.prototype;
-	tqproto.add = function(fn){
+	tqproto.add = function(fn, scope){		
 		fn =  (fn instanceof Array) ? fn : [fn];
+		var fnDef = {};
 		for(var i = 0; i < fn.length; i++){			
 			if(typeof fn[i] === 'function'){
-				this.fnQueue.push(fn[i]);
+				//this.fnQueue.push(fn[i]);
+				fnDef = {
+					fn : fn[i],
+					scope : scope
+				}
+				this.fnQueue.fnDef.push(fnDef);
 			}
 			else{
 				throw "Invalid object: Function expected";
@@ -19,11 +27,12 @@ var timeoutQueue = (function(){
 	tqproto.execute = function(timeout, scope, fnCallback){		
 		var me = this;
 		if(!this.synchronous){
-			if(me.fnQueue && me.fnQueue.length > 0){
+			var fDef, fnDef = me.fnQueue.fnDef;
+			if( fnDef && fnDef.length > 0){
 				var fn;
 				scope = scope || me;
 				
-				var fnCnt = me.fnQueue.length;
+				var fnCnt = fnDef.length;
 				var fnExecCmpl = {
 					success : 0,
 					fail : 0
@@ -44,8 +53,10 @@ var timeoutQueue = (function(){
 						}
 					}
 				}
-				while(me.fnQueue.length > 0){
-					fn = me.fnQueue.shift();
+				while(fnDef.length > 0){
+					fDef = fnDef.shift();
+					fn = fDef.fn;
+					scope = fDef.scope || scope;
 					//setTimeout(fn.bind(scope), timeout);
 					(function(fn, scope){
 						setTimeout(function(){
